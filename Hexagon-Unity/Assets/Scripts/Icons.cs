d787447e24e9683cbef6b4632f43947dfe9e8a7c
cboustros <46546409+christopher-boustros@users.noreply.github.com>
2021-09-25 22:14:52 -0400

    Add scripts

---
+++ Hexagon-Unity/Assets/Scripts/Icons.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Icons : MonoBehaviour
+{
+    // The text objects that display the balance of water, oxygen, and energy resources
+    public Text waterBalanceText;
+    public Text oxygenBalanceText;
+    public Text energyBalanceText;
+
+    // Variables to store the balance of water, oxygen, and energy resources
+    private static int waterBalance = 0;
+    private static int oxygenBalance = 0;
+    private static int energyBalance = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetWaterBalance(1);
+        SetOxygenBalance(2);
+        SetEnergyBalance(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update the text
+        waterBalanceText.text = waterBalance.ToString();
+        oxygenBalanceText.text = oxygenBalance.ToString();
+        energyBalanceText.text = energyBalance.ToString();
+    }
+
+    /*
+     * Getter and setter methods
+     */
+
+    public static int GetWaterBalance()
+    {
+        return waterBalance;
+    }
+
+    public static void SetWaterBalance(int balance)
+    {
+        waterBalance = balance;
+    }
+
+    public static int GetOxygenBalance()
+    {
+        return oxygenBalance;
+    }
+
+    public static void SetOxygenBalance(int balance)
+    {
+       oxygenBalance = balance;
+    }
+
+    public static int GetEnergyBalance()
+    {
+        return energyBalance;
+    }
+
+    public static void SetEnergyBalance(int balance)
+    {
+        energyBalance = balance;
+    }
+}
