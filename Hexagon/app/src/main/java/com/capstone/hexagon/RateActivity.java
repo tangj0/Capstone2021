1a734063810c566e44da17f4489381f0c33bb60d
tangj0 <jtang186@gmail.com>
2021-10-13 13:23:26 -0400

    rating with contribution finalRating update

--- Hexagon/app/src/main/java/com/capstone/hexagon/RateActivity.java
+++ Hexagon/app/src/main/java/com/capstone/hexagon/RateActivity.java
@@ -51,7 +51,9 @@ public class RateActivity extends AppCompatActivity implements View.OnClickListe
 
     private Contribution contToRate;
     private Rating rating;
+    private final int MAX_NUM_OF_RATINGS = 5; //TODO: change to 21
     private List<Contribution> contributions;
+    private List<Rating> ratings;
     private List<Contribution> contsToRemove;
     private List<Boolean> equalsCurr;
 
@@ -91,15 +93,25 @@ public class RateActivity extends AppCompatActivity implements View.OnClickListe
         contributions = new ArrayList<>();
         contsToRemove = new ArrayList<>();
         rating = new Rating();
+        ratings = new ArrayList<>();
 
+        getAllContributions();
+    }
+
+    // get all contributions from the "unrated" pile, from all players
+    // reference: https://stackoverflow.com/questions/48027696/firestore-task-whenallcomplete-task-is-not-yet-complete
+    // Continuation<> explanation: https://stackoverflow.com/questions/40161333/google-play-services-task-api-continuewith-vs-continuewithtask
+    public void getAllContributions() {
         CollectionReference collectionReference = fStore.collection("contributions");
 
-        // get all contributions from the "unrated" pile, from all players
+        // Continuation<input, Task<output>>, task = input, tasks = output
         collectionReference.get()
                 .continueWithTask(new Continuation<QuerySnapshot, Task<List<QuerySnapshot>>>() {
                     @Override
                     public Task<List<QuerySnapshot>> then(@NonNull Task<QuerySnapshot> task) {
                         List<Task<QuerySnapshot>> tasks = new ArrayList<Task<QuerySnapshot>>();
+
+                        // getting each of the "unrated" collections from each contribution, hence the result is a List<QuerySnapshot>
                         for (DocumentSnapshot documentSnapshot : task.getResult()){
                             tasks.add(documentSnapshot.getReference().collection("unrated").get());
                         }
@@ -112,6 +124,7 @@ public class RateActivity extends AppCompatActivity implements View.OnClickListe
                 for(QuerySnapshot querySnapshot : list) {
                     for (DocumentSnapshot documentSnapshot : querySnapshot) {
                         Contribution contribution = documentSnapshot.toObject(Contribution.class);
+
                         // add everything to list for now, filter out current player later
                         // otherwise null pointer exception, need to finish getting contribution first
                         contributions.add(contribution);
@@ -144,6 +157,7 @@ public class RateActivity extends AppCompatActivity implements View.OnClickListe
 
             // TODO: set contToRate images
             // TODO: in future, sort contributions by date (first come first serve)
+            // TODO: prevent players from getting a new contribution without submitting the first rating
         }
 
     }
@@ -152,7 +166,7 @@ public class RateActivity extends AppCompatActivity implements View.OnClickListe
         // set current rating
         UUID ratingId = UUID.randomUUID();
         String strRatingId = ratingId.toString();
-        if (contToRate != null) {
+        if (contToRate != null && tvGarbageType.getText() != "") {
             rating.setId(strRatingId);
 
             rating.setRaterId(playerId);
@@ -164,63 +178,140 @@ public class RateActivity extends AppCompatActivity implements View.OnClickListe
                 rating.setApproval(false);
             }
             rating.setComment(etComment.getText().toString());
+
+
+            // submit current rating
+            final ProgressDialog pd = new ProgressDialog(this);
+            pd.setMessage("Uploading");
+            pd.show();
+
+            DocumentReference ratingDocumentReference = fStore.collection("contributions")
+                    .document(contToRate.getPlayerId())
+                    .collection("unrated")
+                    .document(contToRate.getId())
+                    .collection("ratings")
+                    .document(strRatingId);
+            ratingDocumentReference.set(rating).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(RateActivity.this, "Rating uploaded successfully!", Toast.LENGTH_LONG).show();
+                        pd.dismiss();
+                    }
+                    else {
+                        Toast.makeText(RateActivity.this, "Rating upload was unsuccessful", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+            //Increment corresponding contribution rating count
+            int numOfRatings = contToRate.getNumOfRatings() + 1;
+            fStore.collection("contributions")
+                    .document(contToRate.getPlayerId())
+                    .collection("unrated")
+                    .document(contToRate.getId()).update("numOfRatings", numOfRatings);
+
+            // automatically clear contToRate UI (don't want same user rating same cont twice)
+            tvGarbageType.setText("");
+            tvGarbageAmount.setText("");
+            // TODO: clear contToRate images
+
+            // if a contribution has enough ratings, compute finalRating and move it to the "rated" pile
+            if (numOfRatings >= MAX_NUM_OF_RATINGS) { //should never be > though
+                calcFinalRating();
+                //moveContribution();
+            }
         }
 
-        // submit current rating
-        final ProgressDialog pd = new ProgressDialog(this);
-        pd.setMessage("Uploading");
-        pd.show();
+    }
 
-        DocumentReference ratingDocumentReference = fStore.collection("contributions")
+    // get all ratings from the current contribution
+    private void calcFinalRating() {
+        CollectionReference collectionReference = fStore.collection("contributions")
                 .document(contToRate.getPlayerId())
                 .collection("unrated")
                 .document(contToRate.getId())
-                .collection("ratings")
-                .document(strRatingId);
-        ratingDocumentReference.set(rating).addOnCompleteListener(new OnCompleteListener<Void>() {
+                .collection("ratings");
+
+        collectionReference.get()
+                .continueWithTask(new Continuation<QuerySnapshot, Task<List<DocumentSnapshot>>>() {
+                    @Override
+                    public Task<List<DocumentSnapshot>> then(@NonNull Task<QuerySnapshot> task) {
+                        List<Task<DocumentSnapshot>> tasks = new ArrayList<Task<DocumentSnapshot>>();
+                        System.out.println("HEREE: " + task.getResult());
+                        for (DocumentSnapshot documentSnapshot : task.getResult()){
+                            tasks.add(documentSnapshot.getReference().get());
+                        }
+                        System.out.println("TASKS: "+ tasks);
+                        System.out.println("TASKS len: "+ tasks.size());
+                        return Tasks.whenAllSuccess(tasks);
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<List<DocumentSnapshot>>() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(RateActivity.this, "Rating uploaded successfully!", Toast.LENGTH_LONG).show();
-                    pd.dismiss();
+            public void onComplete(@NonNull Task<List<DocumentSnapshot>> task) {
+                List<DocumentSnapshot> list = task.getResult();
+
+                // average all ratings
+                for(DocumentSnapshot documentSnapshot : list) {
+                    Rating rating = documentSnapshot.toObject(Rating.class);
+                    System.out.println("RATING: " + rating);
+                    ratings.add(rating);
                 }
-                else {
-                    Toast.makeText(RateActivity.this, "Rating upload was unsuccessful", Toast.LENGTH_LONG).show();
+
+                System.out.println("RATINGS: " + ratings);
+                int trueCount = 0;
+                for (Rating rating : ratings) {
+                    if (rating.isApproval()) {
+                        trueCount++;
+                    }
                 }
+                System.out.println("TRUE count: " + trueCount);
+                boolean finalRating = ((float) trueCount / (float) MAX_NUM_OF_RATINGS) > 0.5;
+                fStore.collection("contributions")
+                        .document(contToRate.getPlayerId())
+                        .collection("unrated")
+                        .document(contToRate.getId()).update("finalRating", finalRating);
+
             }
         });
 
-        //Increment corresponding contribution rating count
-        fStore.collection("contributions")
-                .document(contToRate.getPlayerId())
-                .collection("unrated")
-                .document(contToRate.getId()).update("numOfRatings", contToRate.getNumOfRatings() + 1);
-
-        //TODO
-        // automatically get the next contribution (don't want same user rating same cont twice)
-
-        //TODO
-        // check if rating count == 20
-        // if true, automatically move this contribution to "rated" pile in database
-
     }
+
+//    public void moveContribution() {
+//        DocumentReference contDocumentReference = fStore.collection("contributions")
+//                .document(contToRate.getPlayerId())
+//                .collection("unrated")
+//                .document(contToRate.getId());
+//        contDocumentReference.get()
+//                .continueWithTask(new Continuation<DocumentSnapshot, Task<? extends Object>>() {
+//                    @Override
+//                    public Task<? extends Object> then(@NonNull @org.jetbrains.annotations.NotNull Task<DocumentSnapshot> task) throws Exception {
+//                        return null;
+//                    }
+//                }).addOnCompleteListener(new OnCompleteListener<TContinuationResult>() {
+//            @Override
+//            public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<TContinuationResult> task) {
+//
+//            }
+//        });
+//    }
 
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnRetrieveContribution){
-            System.out.println(contributions);
+//            System.out.println(contributions);
             getContribution();
-            System.out.println(contsToRemove);
-            System.out.println("To RATE: " + contToRate);
+//            System.out.println(contsToRemove);
+//            System.out.println("To RATE: " + contToRate);
         }
         else if (v.getId() == R.id.btnSubmitRating) {
             submitRating();
-            System.out.println("RATING: " + rating);
-            System.out.println(rating.getComment().toString());
-            System.out.println(rating.getId());
-            System.out.println(rating.getRaterId());
-            System.out.println(rating.isApproval());
+//            System.out.println("RATING: " + rating);
+//            System.out.println(rating.getComment().toString());
+//            System.out.println(rating.getId());
+//            System.out.println(rating.getRaterId());
+//            System.out.println(rating.isApproval());
         }
     }
 }
