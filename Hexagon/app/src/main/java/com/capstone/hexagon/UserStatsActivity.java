15d70a498b4bd5d40c0069149aac34d4c46c4498
Glen <jiayuan.xu@mail.mcgill.ca>
2021-11-12 21:18:07 -0500

    halfway

--- Hexagon/app/src/main/java/com/capstone/hexagon/UserStatsActivity.java
+++ Hexagon/app/src/main/java/com/capstone/hexagon/UserStatsActivity.java
@@ -13,7 +13,18 @@ import com.google.firebase.auth.FirebaseAuth;
 public class UserStatsActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button go_back_to_main;
-    private TextView title;
+
+    private TextView user;
+    private TextView time;
+    private TextView place;
+
+    private TextView mon;
+    private TextView tue;
+    private TextView wed;
+    private TextView thu;
+    private TextView fri;
+    private TextView sat;
+    private TextView sun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,29 @@ public class UserStatsActivity extends AppCompatActivity implements View.OnClick
 
         go_back_to_main = (Button) findViewById(R.id.back_button);
         go_back_to_main.setOnClickListener(this);
+
+        user = (TextView) findViewById(R.id.user);
+        time = (TextView) findViewById(R.id.time);
+        place = (TextView) findViewById(R.id.space);
+
+        mon = (TextView) findViewById(R.id.mon);
+        tue = (TextView) findViewById(R.id.tue);
+        wed = (TextView) findViewById(R.id.wed);
+        thu = (TextView) findViewById(R.id.thu);
+        fri = (TextView) findViewById(R.id.fri);
+        sat = (TextView) findViewById(R.id.sat);
+        sun = (TextView) findViewById(R.id.sun);
+
+        mon.setText("MON");
+        tue.setText("TUE");
+        wed.setText("WED");
+        thu.setText("THU");
+        fri.setText("FRI");
+        sat.setText("SAT");
+        sun.setText("SUN");
+
+
+
     }
 
     @Override
