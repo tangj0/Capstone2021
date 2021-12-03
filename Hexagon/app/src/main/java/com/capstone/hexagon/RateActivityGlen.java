6e17f87efbee47219ea1bac6cef49ac92202bbe2
Glen <jiayuan.xu@mail.mcgill.ca>
2021-12-03 12:42:48 -0500

    Update RateActivityGlen.java

--- Hexagon/app/src/main/java/com/capstone/hexagon/RateActivityGlen.java
+++ Hexagon/app/src/main/java/com/capstone/hexagon/RateActivityGlen.java
@@ -1,6 +1,7 @@
 package com.capstone.hexagon;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
